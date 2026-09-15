dc02ed21b331f18a8ad2fc6d4d69f1f9d57cb8b3
Paul Applegate <paul@theapplegates.com>
2026-09-15 09:59:42 -0400

    redirect from “splash Screen”

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin()
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en-US',
+        permanent: true, // or false if you plan on changing default locales dynamically
+      },
+    ]
+  },
   reactCompiler: true,
   transpilePackages: ['next-mdx-remote'],
 }
